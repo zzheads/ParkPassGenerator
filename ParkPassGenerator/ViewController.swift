97e8cd04758443059e6adc04afe5ef3d6208cde3
Alexey Papin <zzheads@gmail.com>
2016-11-26 16:49:00 +0300

    Final commit, just for pass "Treehouse detect changes system :)"

--- ParkPassGenerator/ViewController.swift
+++ ParkPassGenerator/ViewController.swift
@@ -12,7 +12,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.        
         
         print("Trying initialize from Fake User Input: ")
         let userInput = UserInput()
@@ -32,7 +31,7 @@ class ViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
     }
 }
 
